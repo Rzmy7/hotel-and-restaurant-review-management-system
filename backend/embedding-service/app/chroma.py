4367323a5a7c2a4209dee86147037cbecbbca260
Poorna Rashmika <poornarashmika7@gmail.com>
2026-01-11 19:19:26 +0530

    update according to vectordb

--- backend/embedding-service/app/chroma.py
+++ backend/embedding-service/app/chroma.py
@@ -10,20 +10,6 @@ client = chromadb.Client(
 
 collection = client.get_or_create_collection("hotel_reviews")
 
-<<<<<<< HEAD
-def save_embedding(review_id: str, embedding, metadata: dict):
-    collection.add(
-        ids=[review_id],
-        embeddings=[embedding],
-        metadatas=[metadata]
-    )
-
-def count_embeddings():
-    return collection.count()
-
-def peek_embeddings(limit=5):
-    return collection.peek(limit=limit)
-=======
 def save_embedding(review_id, embedding, metadata, document=None):
     collection.add(
         ids=[review_id],
@@ -31,4 +17,3 @@ def save_embedding(review_id, embedding, metadata, document=None):
         metadatas=[metadata],
         documents=[document] if document else None
     )
->>>>>>> vectordb
